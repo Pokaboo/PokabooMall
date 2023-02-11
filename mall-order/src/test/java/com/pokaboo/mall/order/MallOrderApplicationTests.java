dfe13dff36bed64c93066ac9c805d03c17a736e9
Pokaboo <1152401192@qq.com>
2023-02-11 21:28:44 +0800

    init project

---
+++ mall-order/src/test/java/com/pokaboo/mall/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokaboo.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
